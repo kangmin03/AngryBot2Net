5f821f23e141d14f7e6f0e7c5a6b0527deff3485
kangmin03 <lkm8276@g.shingu.ac.kr>
2023-11-28 22:55:32 +0900

    xr겜프 12주차

--- AngryBot2Net_1-B/Assets/Scripts/Player.cs
+++ AngryBot2Net_1-B/Assets/Scripts/Player.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Cinemachine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, IPunObservable
 {
     // 컴포넌트 캐시 처리를 위한 변수
     private CharacterController controller;
     private new Transform transform;
     private Animator animator;
     private new Camera camera;
+    private PhotonView pv;
+    // 시네머신 가상 카메라를 저장할 변수
+    private CinemachineVirtualCamera virtualCamera;
     // 가상의 Plane에 레이캐스팅하기 위한 변수
     private Plane plane;
     private Ray ray;
     private Vector3 hitPoint;
+    // 수신된 위치와 회전값을 저장할 변수
+    private Vector3 receivePos;
+    private Quaternion receiveRot;
+    // 수신된 좌표로 이동 및 회전 속도의 민감도
+    public float damping = 10.0f;
     // 이동 속도
     public float moveSpeed = 10.0f;
     void Start()
@@ -21,14 +32,41 @@ public class Player : MonoBehaviour
         transform = GetComponent<Transform>();
         animator = GetComponent<Animator>();
         camera = Camera.main;
+
+        pv = GetComponent<PhotonView>();
+        virtualCamera = GameObject.FindObjectOfType<CinemachineVirtualCamera>();
+        //PhotonView가 자신의 것일 경우 시네머신 가상카메라를 연결
+        if (pv.IsMine)
+        {
+            virtualCamera.Follow = transform;
+            virtualCamera.LookAt = transform;
+        }
+
         // 가상의 바닥을 주인공의 위치를 기준으로 생성
         plane = new Plane(transform.up, transform.position);
         Debug.Log(plane);
     }
     void Update()
     {
-        Move();
-        Turn();
+        // 자신이 생성한 네트워크 객체만 컨트롤
+        if (pv.IsMine)
+        {
+            Move();
+            Turn();
+        }
+        else
+        {
+            // 수신된 좌표로 보간한 이동처리
+            transform.position = Vector3.Lerp(transform.position,
+            receivePos,
+            Time.deltaTime * damping);
+
+            // 수신된 회전값으로 보간한 회전처리
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+            receiveRot,
+            Time.deltaTime * damping);
+
+        }
     }
 
     // 키보드 입력값 연결
@@ -69,5 +107,20 @@ public class Player : MonoBehaviour
         lookDir.y = 0;
         // 주인공 캐릭터의 회전값 지정
         transform.localRotation = Quaternion.LookRotation(lookDir);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        // 자신의 로컬 캐릭터인 경우 자신의 데이터를 다른 네트워크 유저에게 송신
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            receivePos = (Vector3)stream.ReceiveNext();
+            receiveRot = (Quaternion)stream.ReceiveNext();
+        }
     }
 }
